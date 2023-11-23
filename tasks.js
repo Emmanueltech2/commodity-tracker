e001c48ed78d5a2fc85bd53c8745c1c00f600ab5
AsinineFatuity <asininefatuity@gmail.com>
2023-11-23 23:42:05 +0300

    worked on due date for followup

--- tasks.js
+++ tasks.js
@@ -6,18 +6,21 @@ module.exports = [
 {
     name: 'follow-up-household-member',
     title: 'Follow up household member',
-    icon: 'icon-follow-up',
+    icon: 'icon-healthcare-assessment',
     appliesTo: 'reports',
     appliesToType: ['household_member_assessment'],
     appliesIf: function(contact, report){
+        console.log('report', report);
         let userHasDangerSigns = getField(report, 'household_member_assessment.initial_symptoms') === 'yes';
         return userHasDangerSigns;
     },
     actions: [{form: 'cholera_follow_up'}],
     events: [{
-        start: 7,
-        end: 0,
-        days: 7,
+        start: 3,
+        end: 1,
+        dueDate: function(event, contact, report){
+            return new Date(report.reported_date + (event.start * 24 * 60 * 60 * 1000));
+        }
     }],
 },
 ];
