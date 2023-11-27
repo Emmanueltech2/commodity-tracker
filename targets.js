a0d3ca4e9fa0f77aed43576710fe9a90d43be237
AsinineFatuity <asininefatuity@gmail.com>
2023-11-27 22:57:01 +0300

    added target for total referrals given and honored

--- targets.js
+++ targets.js
@@ -4,6 +4,21 @@ const {
 } = extras;
 module.exports = [
     {
+        id: 'register_households_target',
+        translation_key:'target.register_households',
+        subtitle_translation_key: 'target.register_households.subtitle',
+        icon: 'icon-register-household',
+        type: 'count',
+        goal: 50,
+        appliesTo: 'contacts',
+        appliesToType: ['household'],
+        appliesIf: function(contact){
+            let isHouseHoldType = contact.contact.contact_type === 'household';
+            return isHouseHoldType;
+        },
+        date: 'now'
+    },
+    {
         id: 'follow_up_household_member_target',
         translation_key:'target.follow_up_household_member',
         subtitle_translation_key: 'target.follow_up_household_member.subtitle',
@@ -21,18 +36,33 @@ module.exports = [
 
     },
     {
-        id: 'register_households_target',
-        translation_key:'target.register_households',
-        subtitle_translation_key: 'target.register_households.subtitle',
-        icon: 'icon-register-household',
+        id:'referrals_given_target',
+        translation_key:'target.referrals_given',
+        subtitle_translation_key: 'target.referrals_given.subtitle',
+        icon: 'icon-referral',
         type: 'count',
-        goal: 50,
-        appliesTo: 'contacts',
-        appliesToType: ['household'],
-        appliesIf: function(contact){
-            let isHouseHoldType = contact.contact.contact_type === 'household';
-            return isHouseHoldType;
+        goal: 20,
+        appliesTo: 'reports',
+        appliesToType: ['household_member_assessment'],
+        appliesIf: function(contact, report){
+            let referralGiven = getField(report, 'household_member_assessment.initial_symptoms');
+            return referralGiven === 'yes';
         },
-        date: 'now'
-    }
+        date: 'reported'
+    },
+    {
+        id:'referrals_honored_target',
+        translation_key:'target.referrals_honored',
+        subtitle_translation_key: 'target.referrals_honored.subtitle',
+        icon: 'icon-referral',
+        type: 'count',
+        goal: 20,
+        appliesTo: 'reports',
+        appliesToType: ['cholera_follow_up'],
+        appliesIf: function(contact, report){
+            let visitedFacility = getField(report, 'danger_signs.visit_confirm');
+            return visitedFacility === 'yes';
+        },
+        date: 'reported'
+    },
 ];
