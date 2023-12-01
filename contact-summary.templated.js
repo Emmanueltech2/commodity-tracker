88d3044d675b679415d532aa72fdbe8efa0937e0
AsinineFatuity <asininefatuity@gmail.com>
2023-12-01 09:43:07 +0300

    improved condition cards

--- contact-summary.templated.js
+++ contact-summary.templated.js
@@ -27,20 +27,33 @@ const householdMemberCards = [
     appliesToType: ['report'],
     appliesIf: (report) => {
         let correctContact = thisContact.contact_type === 'household_member' || thisContact.contact_type === 'household_contact';
-        let correctForm = report.form === 'household_member_assessment';
-        let isFormLatest = report === getMostRecentReport(allReports, 'household_member_assessment');
-        return correctContact && correctForm && isFormLatest;
+        let correctForm = report.form === 'household_member_assessment' || report.form === 'cholera_verification';
+        let isAssessmentFormLatest = report === getMostRecentReport(allReports, 'household_member_assessment');
+        let isCholeraFormLatest = report === getMostRecentReport(allReports, 'cholera_verification');
+        return correctContact && correctForm && (isAssessmentFormLatest || isCholeraFormLatest);
       },
     fields: [
       {
-        label: 'Health Condition',
+        label: 'Assessment Condition',
         icon: 'health-condition',
+        appliesIf: (report) => report.form === 'household_member_assessment',
         value: function(report){
           let dangerSignsPresent = report.fields.household_member_assessment.initial_symptoms;
           let healthCondition = dangerSignsPresent === 'yes' ? 'suspicious cholera case' : 'no signs of cholera';
           return healthCondition;
         },
-        width: 12
+        width: 6
+      },
+      {
+        label: 'Cholera Verification',
+        icon: 'cholera-verification',
+        appliesIf: (report) => report.form === 'cholera_verification',
+        value: function(report){
+          let choleraVerification = report.fields.danger_signs.confirm_case;
+          let status = choleraVerification === 'yes' ? 'confirmed cholera case' : 'not a cholera case';
+          return status;
+        },
+        width: 6
       },
     ]
   }
