e084b6db0b98f0452245f354af28617b7148f3ce
AsinineFatuity <asininefatuity@gmail.com>
2023-12-06 19:37:07 +0300

    modularized app tasks

---
+++ app_tasks/index.js
@@ -0,0 +1,9 @@
+const chpTasksExtras = require('./chp_tasks');
+const chaTasksExtras = require('./cha_tasks');
+const {chpTasks} = chpTasksExtras;
+const {chaTasks} = chaTasksExtras;
+
+module.exports = {
+    chpTasks,
+    chaTasks
+};
