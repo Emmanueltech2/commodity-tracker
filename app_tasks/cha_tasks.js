a79f1984ed862f92c0883e7511886e1423c8abbf
AsinineFatuity <asininefatuity@gmail.com>
2023-12-09 23:21:26 +0300

    allowed cha to verify death report for household member

--- app_tasks/cha_tasks.js
+++ app_tasks/cha_tasks.js
@@ -24,6 +24,25 @@ let chaTasks = [
     }],
     priority: {level: 'high', label: 'High Priority'},
 },
+{
+    name: 'let-cha-verify-death',
+    title: 'Verify Death Report',
+    icon: 'icon-death-general',
+    appliesTo: 'reports',
+    appliesToType: ['death_report'],
+    appliesIf: function(contact, report){
+        return report.form === 'death_report' && user.contact_type === 'area_community_health_supervisor';
+    },
+    actions: [{form: 'cha_verify_death', label: 'Verify Death'}],
+    events: [{
+        start: 3,
+        end: 3,
+        dueDate: function(event, contact, report){
+            return new Date(report.reported_date + (event.start * 24 * 60 * 60 * 1000));
+        }
+    }],
+    priority: {level: 'high', label: 'High Priority'},
+}
 ];
 
 module.exports = {chaTasks};
